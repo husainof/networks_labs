c20e01f01a87a5fbfd546e00f55923a3ec5508c6
radik <linuxoid.ssh@gmail.com>
2023-04-14 02:23:28 +0400

    done

---
+++ app.py
@@ -0,0 +1,39 @@
+import ftplib
+import json
+import pprint
+
+
+def traverse(ftp, depth=0):
+    if depth > 10:
+        return ['depth > 10']
+    level = {}
+    for entry in (path for path in ftp.nlst() if path not in ('.', '..')):
+        try:
+            ftp.cwd(entry)
+            level[entry] = traverse(ftp, depth+1)
+            ftp.cwd('..')
+        except ftplib.error_perm:
+            level[entry] = None
+    return level
+
+
+def generate_tree(tree, n=0):
+    for el in tree:
+        if tree[el] is None:
+            print('    |' * n + '-' * 4 + el + '\n')
+
+        elif len(tree[el]):
+            print('    |' * n + '-' * 4 + el + '\\' + '\n')
+            generate_tree(tree[el], n+1)
+
+
+def main():
+    ftp = ftplib.FTP('91.222.128.11')
+    ftp.login('testftp_guest', '12345')
+    tree = traverse(ftp, 4)
+    generate_tree(tree)
+    # pprint.pprint(tree)
+
+
+if __name__ == '__main__':
+    main()
